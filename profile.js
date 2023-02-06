f54b2f9163057119567818cc5022ad4f2d2bcef7
Gisselle Garcia <gissellegar46@gmail.com>
2023-02-06 11:57:42 -0800

    Made changes to formatting to avoid crowding

--- profile.js
+++ profile.js
@@ -1,24 +1,7 @@
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-    
-//     const name = document.getElementById("name").value.trim();
-//     const email = document.getElementById("email");
-//     let emailText = email.value.trim(); //variable for the inner text  (I need the const to set the attribute)
-//     const address = document.getElementById("address");
-//     let addressText  =  password.value.trim();   //made another variable for password text
-      
-//     name.setAttribute("required", "required"); 
-//     email.setAttribute("required", "required");  //made email required with attribute
-
-  
-//   })
-
-
 let button = document.getElementById("submit");
 button.addEventListener("click", outputValues);
 
 function outputValues(){
-  let originalvalue = document.getElementById("username").value;
+  let originalvalue = document.getElementById("name").value;
   console.log(originalvalue);
-  console.log("Dave");
 }
