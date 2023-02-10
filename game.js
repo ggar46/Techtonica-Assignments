66ad78a8939bd976ccecc52d8a49ba908e1920d8
Gisselle Garcia <gissellegar46@gmail.com>
2023-02-09 18:00:49 -0800

    Selected elements and set innerHTML

--- game.js
+++ game.js
@@ -1,19 +1,35 @@
 let flipbutton = document.querySelector("#flipbutton");
+
 let resetbutton = document.querySelector("#resetbutton");
-document.getElementById("picdiv").style.backgroundColor = "blue";
+
+let answerspace = document.querySelector("#answerdiv");
+answerspace.innerHTML = "";
+
+let divpic = document.querySelector("#picdiv");
+
+let guessSpace = document.querySelector("#guessdiv");
+guessSpace.innerHTML = "";
+
+let ele = document.getElementsByName('guessinput');
 
 
-let heads = 0;
-let tails = 0;
 
-//made function to disable buttonn for a few seconds
-function disableButton(){
 
-}
 
-//multiply times 2 because you want number between 0 and 2 --> Math.floor(Math.random()*2)
-//now we need background to turn green and say congrats
-//radio button input to compare to randomized inside the function
+
+
+
+// let heads = 0;
+// let tails = 0;
+
+// //made function to disable buttonn for a few seconds
+// function disableButton(){
+
+// }
+
+// //multiply times 2 because you want number between 0 and 2 --> Math.floor(Math.random()*2)
+// //now we need background to turn green and say congrats
+// //radio button input to compare to randomized inside the function
 // flipbutton.addEventListener("click", () => {
 //     let num0to2 = Math.floor(Math.random()*2);
 //     if(num0to2 == 0){
@@ -24,6 +40,6 @@ function disableButton(){
 // });
 
 
-//maka variables for each one
-const headsguess = document.getElementByQuerySelector("#heads");
-const tailsguess = document.getElementByQuerySelector("#tails");
+// //maka variables for each one
+// const headsguess = document.getElementByQuerySelector("#heads");
+// const tailsguess = document.getElementByQuerySelector("#tails");
