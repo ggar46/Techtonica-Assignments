5bd068dd02fe1dc773c0d3dcee1bc7f59ade34e8
Gisselle Garcia <gissellegar46@gmail.com>
2023-02-17 12:18:47 -0800

    Added images and imported image component

--- rock-paper-scissors-file/src/App.js
+++ rock-paper-scissors-file/src/App.js
@@ -1,5 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Images from "./components/Images";
+
+const image = [
+  "https://i0.wp.com/wrpsa.com/wp-content/uploads/2021/08/rock.png?resize=413%2C386&ssl=1",
+  "https://i0.wp.com/wrpsa.com/wp-content/uploads/2021/08/paper.png?resize=413%2C386&ssl=1",
+  "https://i0.wp.com/wrpsa.com/wp-content/uploads/2021/08/scissors.png?resize=413%2C386&ssl=1"
+];
 
 function App() {
   return (
