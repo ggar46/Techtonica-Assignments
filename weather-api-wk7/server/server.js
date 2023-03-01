e84a35babc97f3b9b05b64b036159a0a5086aa3c
Gisselle Garcia <gissellegar46@gmail.com>
2023-02-28 22:22:06 -0800

    Make get request to acecess desired hardcoded data

--- weather-api-wk7/server/server.js
+++ weather-api-wk7/server/server.js
@@ -1,25 +1,31 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const dataWeather = require("./data")
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const dataWeather = require("./data");
 
 const app = express();
 
-const PORT = 8080;
+const PORT = 8081;
 app.use(cors());
 app.use(express.json());
 
 // creates an endpoint for the route /api
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello from My template ExpressJS' });
-  });
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from My template ExpressJS" });
+});
 
 // creates an endpoint for the route /api/weather
-app.get('/api/weather', (req, res) => {
-  res.json(dataWeather);
+app.get("/api/weather/", (req, res) => {
+  //const //cityName is a query parameter uses ?,  route parameter uses :
+  res.json({
+      name: dataWeather.weather[0].name,
+      temp: dataWeather.weather[0].main.temp,
+      weather: dataWeather.weather[0].main.pressure,
+      clouds: dataWeather.weather[0].clouds.all,
+      sunrise: dataWeather.weather[0].sys.sunrise,
+      sunset: dataWeather.weather[0].sys.sunset
+    });
 });
 
 // console.log that your server is up and running
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+app.listen(PORT, () => console.log(`Hola! Server is running on PORT ${PORT}`));
