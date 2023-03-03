40a886f272f425711d1caed366eafcf35c9bd735
Gisselle Garcia <gissellegar46@gmail.com>
2023-03-03 12:07:28 -0800

    Define handleSubmit in the parent as equal to the handleSubmit function in App.js

--- weather-api-wk7/client/src/App.js
+++ weather-api-wk7/client/src/App.js
@@ -4,30 +4,33 @@ import WeatherForm from './components/weatherForm';
 import WeatherCard from './components/weatherCard';
 
 function App() {
-  const [city, setCity] = useState("");
+  //const [city, setCity] = useState("");
   const [result, setResult] = useState(null);
 
   //A function to do the get request and set the state from the hard code data
-  const loadCity = () => {
-    fetch("http://localhost:8080/api/weather")
-      .then((response) => response.json())
-      .then((result) => {
-        //console.log(result)
-        setCity(result.weather[0].name);
-        setResult(result);
-      });
+  const loadCity = (city) => {
+    // fetch(`http://localhost:8081/api/weather/${city}`)
+    //   .then((response) => response.json())
+    //   .then((result) => {
+    //     //console.log(result)
+    //     // setCity(result.weather[0].name);
+    //     console.log("result=", result);
+    //     setResult(result);
+    //   }
+      
+    //   );
   }
 
- const handleSubmit = (e) =>{
-  e.preventDefault();
-  loadCity();
+ const handleSubmit = (e, city) =>{
+  // e.preventDefault();
+  // loadCity(city);
  }
 
 
   return (
     <div className="App">
-      <WeatherForm city={city} handleSubmit={handleSubmit}/>
-      {!result ? <p>Please click the botton to see Data</p> : <WeatherCard data={result} /> }
+      {/* <WeatherForm handleSubmit={handleSubmit}/> */}
+      {/* {!result ? <p>Please click the botton to see Data</p> : <WeatherCard data={result} /> } */}
     </div>
   );
 }
