d01c2fc11b2eaa6883bf47294b8cdaf77e69ab7d
Gisselle Garcia <gissellegar46@gmail.com>
2023-03-03 11:39:09 -0800

    Connect to API with fetch function

--- weather-api-wk7/server/server.js
+++ weather-api-wk7/server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const dataWeather = require("./data");
+const coord = require("./data");
 
 const app = express();
 
@@ -14,18 +15,38 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello from My template ExpressJS" });
 });
 
-// creates an endpoint for the route /api/weather
-app.get("/api/weather/", (req, res) => {
-  //const //cityName is a query parameter uses ?,  route parameter uses :
-  res.json({
-      name: dataWeather.weather[0].name,
-      temp: dataWeather.weather[0].main.temp,
-      weather: dataWeather.weather[0].main.pressure,
-      clouds: dataWeather.weather[0].clouds.all,
-      sunrise: dataWeather.weather[0].sys.sunrise,
-      sunset: dataWeather.weather[0].sys.sunset
-    });
+
+app.get("/api/weather/:city", (req, res) => {
+  console.log("code reached here");
+  const requestedCity = req.params.city;
+  
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${requestedCity}`)
+      
 });
+
+
+
+
+
+
+
+
+//works
+// app.get("/api/weather/:city", (req, res) => {
+//   console.log("code reached here");
+//   const requestedCity = req.params.city;
+  
+//   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${requestedCity}&appid=df4dc696102dd6129092d84b487c1aaa&units=imperial`)
+//     //res  is what we will respond, and respond refers to the api response
+//     .then(async (data) => {
+
+//         const weather = await data.json();
+//         res.json(weather);
+        
+// });
+      
+// });
+
 
 // console.log that your server is up and running
 app.listen(PORT, () => console.log(`Hola! Server is running on PORT ${PORT}`));
