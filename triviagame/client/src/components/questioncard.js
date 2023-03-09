d0538b72d624fb5a2a0f7f843d9c38ecc09aea7f
Gisselle Garcia <gissellegar46@gmail.com>
2023-03-09 15:23:48 -0800

    Disable buttons after they are clicked, start planning counter to get score

--- triviagame/client/src/components/questioncard.js
+++ triviagame/client/src/components/questioncard.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./questioncard.css";
 
+
 const QuestionCard = (props) => {
     /*
     create state, onClick function, function that passes button data, and
@@ -8,9 +9,18 @@ const QuestionCard = (props) => {
     console.log the string with true/false*/
    
   const [answer, changeAnswer] = useState("");
+  const [status, changeStatus] = useState(false);
+  //let [count, setCount] = useState(0);
   
 const handleAnswer = (buttonAnswer) => {
-  (buttonAnswer === props.question.correct_answer) ?  changeAnswer("True") : changeAnswer("False");
+  if(buttonAnswer === props.question.correct_answer) {
+    changeAnswer("True");
+    changeStatus(true);
+    //setCount(count+1);
+  } else {
+    changeAnswer("False");
+    changeStatus(true);
+  }
 }
 
 
@@ -19,12 +29,18 @@ const handleAnswer = (buttonAnswer) => {
         <div className={answer === "" ? "no-color": ((answer === "True") ? "green-div": "red-div")}>
         <div className='question-text'>{props.question.question}</div>
         <div className='answer-section'>
-		<button onClick={() => handleAnswer("True")}>True</button>
-		<button onClick={() => handleAnswer("False")}>False</button>
+		<button disabled={status} onClick={() => handleAnswer("True")}>True</button>
+		<button disabled={status} onClick={() => handleAnswer("False")}>False</button>
         </div>
-        {answer === "" ? <p></p>:((answer === "True") ? <p>Correct</p> : <p>Incorrect</p>)}
+        {/* {count} */}
+        {/* {answer === "" ? <p></p>:((answer === "True") ? <p>Correct</p> : <p>Incorrect</p>)} */}
       </div>
     );
   };
 
 export default QuestionCard;
+
+/*
+- now we want the count to count all of them and keep adding up instead of just counting one
+- or we can add to map?
+*/
